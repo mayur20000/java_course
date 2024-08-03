912925e0ca72cef9c4a271eba378f3aa779f69bf
Mayur Vishwakarma <mayurvish2003@gmail.com>
2024-08-04 01:28:49 +0530

    checking the command by pushing it by git push without setting up the upstream

--- printPractise.java
+++ printPractise.java
@@ -2,6 +2,7 @@ package BasicsOfJava;
 
 public class printPractise {
     public static void main(String[] args) {
-        System.out.println("Heelo This is the test message to check git remote repository update");
+        System.out.println("Hello This is the test message to check git remote repository update");
+        System.out.println("checking another command to test directly with git push");
     }
 }
