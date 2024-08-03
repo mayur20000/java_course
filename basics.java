8eda77659982f174f71bffa4a29979543b8fcb0f
mayur20000 <mayurvish2003@gmail.com>
2024-08-04 01:14:14 +0530

    new files added

--- basics.java
+++ basics.java
@@ -4,6 +4,11 @@ public class basics{
     public static void main(String[] args){
         System.out.println("Hello Mayur");
         System.out.println("Hello Mayur Vishwakarma");
+        System.out.println("hello World");
+        System.out.print("four + three = ");
+        System.out.println(4+3);
+        int sum = 5+6;
+        System.out.println(sum);
 
     }
 
---
+++ variables.java
@@ -0,0 +1,14 @@
+package BasicsOfJava;
+
+public class variables {
+    public static void main(String[] args){
+        System.out.println();
+        int age = 20;
+        System.out.println(age);
+
+
+
+
+
+    }
+}
