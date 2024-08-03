b4d47863dd0ffa0669e79205b2be863b774961a7
Mayur Vishwakarma <mayurvish2003@gmail.com>
2024-08-04 01:24:41 +0530

    new dile is added to check the update remote git repository

---
+++ printPractise.java
@@ -0,0 +1,7 @@
+package BasicsOfJava;
+
+public class printPractise {
+    public static void main(String[] args) {
+        System.out.println("Heelo This is the test message to check git remote repository update");
+    }
+}
