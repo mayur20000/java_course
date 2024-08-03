03908357ccfb27a7cfb40eed5756683839b1805b
mayur20000 <mayurvish2003@gmail.com>
2024-08-03 20:15:01 +0530

    first program added

---
+++ basics.java
@@ -0,0 +1,10 @@
+package BasicsOfJava;
+
+public class basics{
+    public static void main(String[] args){
+        System.out.println("Hello Mayur");
+        System.out.println("Hello Mayur Vishwakarma");
+
+    }
+
+}
