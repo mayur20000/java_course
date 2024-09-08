7fe9510f50d4a3794e7c4d84b9e4b8a5af162132
mayur20000 <mayurvish2003@gmail.com>
2024-09-08 19:10:40 +0530

    airth add

---
+++ Opr/Arith.java
@@ -0,0 +1,18 @@
+package BasicsOfJava.Opr;
+
+public class Arith {
+
+        public static void main() {
+            int p = 25, q = 10;
+            System.out.println(p+q);  // Addition
+            System.out.println(p-q);  // Subtraction
+            System.out.println(p*q);  // Multiplication
+            System.out.println(p/q);  // Division
+            System.out.println(p%q);  // Modulas
+
+        }
+    }
+
+
+
+
