7b011278f1a5a0d3b55527f0bf85bfdf930f5318
mayur20000 <mayurvish2003@gmail.com>
2024-09-08 19:13:51 +0530

    relational

---
+++ Relational/Comparison.java
@@ -0,0 +1,16 @@
+package BasicsOfJava.Relational;
+
+public class Comparison {
+
+        public static void main() {
+            int p=15, q=10;
+
+            System.out.println(p==q ); // is equals to
+            System.out.println(p!=q );// not equals to
+            System.out.println(p<q );// smaller than
+            System.out.println(p>q );//  greater than
+            System.out.println(p<=q );// smaller than or equals to
+            System.out.println(p>=q );// greater than or equals to
+
+        }
+    }
