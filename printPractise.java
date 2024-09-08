472d30c284b03758612daa2045baf821650b0800
mayur20000 <mayurvish2003@gmail.com>
2024-09-08 18:59:30 +0530

    modified

--- printPractise.java
+++ printPractise.java
@@ -4,5 +4,6 @@ public class printPractise {
     public static void main() {
         System.out.println("Hello This is the test message to check git remote repository update");
         System.out.println("checking another command to test directly with git push");
+        System.out.println("checking another command to test directly with git push");
     }
 }
