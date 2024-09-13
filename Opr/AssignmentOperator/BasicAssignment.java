2ae770775d356dc4bd72e56cb3ea69371a3591a9
mayur20000 <mayurvish2003@gmail.com>
2024-09-13 16:01:13 +0530

    BasicAssignment

---
+++ Opr/AssignmentOperator/BasicAssignment.java
@@ -0,0 +1,11 @@
+// Assign the value 10 to a variable x and print it.
+package BasicsOfJava.Opr.AssignmentOperator;
+
+public class BasicAssignment {
+    public static void main(String[] args) {
+
+        int x = 10;
+        System.out.println(x);
+    }
+
+}
