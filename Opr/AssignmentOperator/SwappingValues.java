b0b1121bd7504e6206e1ffc590025ccc45995fb2
mayur20000 <mayurvish2003@gmail.com>
2024-09-13 16:27:31 +0530

    Swapping Values

---
+++ Opr/AssignmentOperator/SwappingValues.java
@@ -0,0 +1,18 @@
+package BasicsOfJava.Opr.AssignmentOperator;
+
+public class SwappingValues {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a :" + a + ", b : " +b);
+
+
+
+    }
+}
