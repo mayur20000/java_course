383fdebe5788b080080a1f6922e32983e91a7035
mayur20000 <mayurvish2003@gmail.com>
2024-09-14 23:07:32 +0530

    Concept.java

---
+++ ConditionalStatements/Else_if_statement/Concept.java
@@ -0,0 +1,33 @@
+//You can also have else if statements. As the name suggests,
+// it is a combination of else and if. Like else,
+// it extends an if statement to execute a different set of statements in case the original
+// if expression evaluates to false. Then, the conditions present inside the else if blocks are checked.
+// Once a condition evaluates to true, remaining else if and else statements are skipped.
+//When all the conditions are false, the else block is executed. Coding the else block is optional.
+
+
+package BasicsOfJava.ConditionalStatements.Else_If_Statement;
+
+public class Concept {
+
+    /*--------------------Syntax
+
+            if (<condition 1>) {
+
+             <statements>;
+            }
+            else if (<condition 2>){
+            <statements>;
+            }
+            else if (<condition 3>){
+            <statements>;
+            }
+            else {
+             <statements>;
+           }
+
+
+
+
+     ---------------------------*/
+}
