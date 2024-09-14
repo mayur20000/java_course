df9026a8f7682090d18bbed8b47c09058ed18cca
mayur20000 <mayurvish2003@gmail.com>
2024-09-14 17:24:51 +0530

    Concept of Control Statement or controls struture or conditional Structure

---
+++ ConditionalStatements/Concept.java
@@ -0,0 +1,30 @@
+//Control Structures in Programming
+//Overview
+//Definition: Control structures manage the flow of a program.
+//Purpose: They determine which instructions are executed and in what order.
+
+// ## Types of Control Structures ##
+
+// -- Sequential: Executes statements line by line.
+// -- Selection: Chooses different paths (e.g., if, else).
+// -- Iteration: Repeats a block of code (e.g., for, while loops).
+// -- Jump: Alters the flow (e.g., break, continue, return).
+
+
+
+
+package BasicsOfJava.ConditionalStatements;
+
+public class Concept {
+
+    /*
+
+    if (<condition>) {  // Curly braces are not required if there is only one statement inside the block
+    <statements>;
+    }
+
+     */
+
+
+
+}
---
+++ ConditionalStatements/If_Demo.java
@@ -0,0 +1,30 @@
+//  While ordering food in "SwiftFood", when a customer orders food items,
+//  you need to calculate the total cost that the customer should pay for the order.
+//  The "Regular Customers" are provided with a 5% discount for their orders.
+//  Here, we are assuming that each food item costs $10. In the condition,
+//  we are checking if the customer type is "Regular". If the condition is true,
+//  the statements within the if block get executed to calculate total cost after discount.
+
+
+package BasicsOfJava.ConditionalStatements;
+
+public class If_Demo {
+
+    public static void main(String [] args){
+
+        String customerType = "Regular";
+        int quantity = 3;
+        float unitPrice = 10;
+        float totalCost = 0;
+        float discount = 5;
+
+        totalCost = quantity * unitPrice;
+
+        if (customerType == "Regular"){
+            totalCost = totalCost -  (totalCost * discount / 100);
+            System.out.println("You are a regular customer and eligible for 5% discount");
+
+        }
+        System.out.println("Total Cost = " +totalCost);
+    }
+}
