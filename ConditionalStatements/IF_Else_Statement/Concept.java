e6c70ae9caf8ad3af52f776250ee9209f313fe88
mayur20000 <mayurvish2003@gmail.com>
2024-09-14 18:29:44 +0530

    If else concept and syntax

---
+++ ConditionalStatements/IF_Else_Statement/Concept.java
@@ -0,0 +1,23 @@
+//An if statement can be written along with an else statement.
+// The condition/expression given in the if statement is checked and
+// set of statements are executed based on the outcome of the condition.
+// If the condition is true, the statements written in if block get executed.
+// If the condition is false, then the statements inside else block get executed.
+
+
+package BasicsOfJava.ConditionalStatements.IF_Else_Statement;
+
+public class Concept {
+    public static void main(String[] args) {
+        /*-----------------------------
+        if (<condition>) {
+                <statements>;
+                    }
+        else {
+                <statements>;
+        }
+
+         -------------------------------*/
+
+    }
+}
