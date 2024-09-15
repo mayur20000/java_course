46748e590e2c9755acf88f6c029ef3b76513de78
mayur20000 <mayurvish2003@gmail.com>
2024-09-15 23:37:49 +0530

    Nested If concept

---
+++ ConditionalStatements/Nested_IF/Concept.java
@@ -0,0 +1,24 @@
+//When an if statement is written within another if statement,
+// it is known as nested if statement. It enables us to test multiple criteria.
+// The inner if block condition gets executed only when the condition of the outer if block evaluates to true.
+
+
+package BasicsOfJava.ConditionalStatements.Nested_IF;
+
+public class Concept {
+
+    /*-----------------------------
+
+    if (<condition 1>) {
+        if (<condition 2>) {
+        <statements>;
+        }
+        else {
+        <statements>;
+        }
+}
+
+
+
+    ------------------------------- */
+}
