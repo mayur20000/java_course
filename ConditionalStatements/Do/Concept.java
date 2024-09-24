0c2e3887c6f4c279d0ce7ffac43e71eb69702f2f
mayur20000 <mayurvish2003@gmail.com>
2024-09-24 16:07:53 +0530

    Do concept and syntax

---
+++ ConditionalStatements/Do/Concept.java
@@ -0,0 +1,17 @@
+//When the loop has to be executed at least once before the condition is checked,
+// do-while loop is used. After the first execution, the loop then gets repeated as long as the condition is true.
+// In case of do-while loop,the condition is tested after executing the code block.
+// Hence, it is called an exit-controlled loop.
+
+package BasicsOfJava.ConditionalStatements.Do;
+
+public class Concept {
+
+    /*
+
+    do {
+    <statements>;
+    {while (<condition>);
+
+     */
+}
