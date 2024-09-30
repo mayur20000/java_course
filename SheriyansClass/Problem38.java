f3e1187bdab1ae1e4f1820535c8f715579e62dc3
mayur20000 <mayurvish2003@gmail.com>
2024-09-30 18:28:47 +0530

    problem no 38

---
+++ SheriyansClass/Problem38.java
@@ -0,0 +1,14 @@
+package BasicsOfJava.SheriyansClass;
+
+public class Problem38 {
+    public static void main(String[] args) {
+
+        int n = 45;
+        int sum = n + 25;
+        while (n != 0) {
+            n = n / 10;
+            System.out.println(n);
+
+        }
+    }
+}
