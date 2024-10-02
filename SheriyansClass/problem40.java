d9bb768c60f312e4b4c1cbd8f498f80866954193
mayur20000 <mayurvish2003@gmail.com>
2024-10-02 18:09:39 +0530

    Problem 40 is of automarophic number solution

---
+++ SheriyansClass/problem40.java
@@ -0,0 +1,22 @@
+package BasicsOfJava.SheriyansClass;
+
+import java.util.Scanner;
+
+public class problem40 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter any number : ");
+        int n = scan.nextInt();
+        int cod = 0;
+        int temp = n;
+        while (n != 0) {
+            cod++;
+            n = n / 10;
+
+        }
+        n = temp;
+        int sq = n * n;
+        int lastDigit = sq % (int) (Math.pow(10, cod));
+        System.out.println(lastDigit == n ? "Automotphic" : "Not Automorphic");
+    }
+}
