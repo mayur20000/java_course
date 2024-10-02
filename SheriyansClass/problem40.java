1db2cb79ccb4074e7677763b51591981f598acd7
mayur20000 <mayurvish2003@gmail.com>
2024-10-02 18:29:08 +0530

    probelm 40 do while

---
+++ SheriyansClass/problem40.java
@@ -0,0 +1,19 @@
+package BasicsOfJava.SheriyansClass;
+
+import java.util.Scanner;
+
+public class problem40 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int ch;
+        do {
+            System.out.println("Hello");
+            System.out.println("Press 1 to repeat the task");
+            ch = scan.nextInt();
+
+        }git
+        while (ch == 1);
+
+    }
+}
