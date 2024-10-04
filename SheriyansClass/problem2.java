789fc70a7b5168e5d8c06aafdcc3ebeaaebd8cbc
mayur20000 <mayurvish2003@gmail.com>
2024-10-04 12:35:35 +0530

    probem 2 - Print the message along with values and sum

---
+++ SheriyansClass/problem2.java
@@ -0,0 +1,18 @@
+package BasicsOfJava.SheriyansClass;
+
+import java.util.Scanner;
+
+public class problem2 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter First number : ");
+        int num1 = scan.nextInt();
+        System.out.println("Enter second Number : ");
+        int num2 = scan.nextInt();
+
+        System.out.println("sum of num1 & num2 = " + (num1 + num2));
+
+
+    }
+}
