08b37325fdd9d45c9855e5f149ef0f27f6589b30
mayur20000 <mayurvish2003@gmail.com>
2024-10-04 12:28:30 +0530

    1 - Sum of two integers

--- SheriyansClass/Problem1.java
+++ SheriyansClass/Problem1.java
@@ -1,8 +1,17 @@
 package BasicsOfJava.SheriyansClass;
 
+import java.util.Scanner;
+
 public class Problem1 {
     public static void main(String[] args) {
 
-        System.out.println(" Hello Mayur ");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter First number : ");
+        int num1 = scan.nextInt();
+        System.out.println("Enter second Number : ");
+        int num2 = scan.nextInt();
+
+        System.out.println("sum = " + (num1 + num2));
+
     }
 }
