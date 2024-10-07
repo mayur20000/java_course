3e099f07aeb3095bfd7bd3b4cc74f52df0c9fae0
mayur20000 <mayurvish2003@gmail.com>
2024-10-07 15:03:39 +0530

    Fiboanchi probelm solves

---
+++ SheriyansClass/Fibbonachi.java
@@ -0,0 +1,19 @@
+package BasicsOfJava.SheriyansClass;
+
+import java.util.Scanner;
+
+public class Fibbonachi {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int first = 0, second = 1;
+        System.out.println(first + " " + second + " ");
+        for (int i = 1; i <= n - 2; i++) {
+
+
+        }
+
+    }
+}
