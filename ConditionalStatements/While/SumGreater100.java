cb306499cacb6a28d09e3b97cfa1e01e2b1c61b7
mayur20000 <mayurvish2003@gmail.com>
2024-10-11 19:17:58 +0530

    reads a sequence of integers from the user and stops by displaying done when the sum exceeds 100

--- ConditionalStatements/While/SumGreater100.java
+++ ConditionalStatements/While/SumGreater100.java
@@ -1,5 +1,21 @@
 package BasicsOfJava.ConditionalStatements.While;
 
+import java.util.Scanner;
+
 public class SumGreater100 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int sum = 0;
+        while (true) {
+            System.out.print("Enter a number : ");
+            sum += scan.nextInt();
+
+            if (sum >= 100) {
+                break;
+            }
+
+        }
+        System.out.println("Done :" + sum);
+    }
 
 }
