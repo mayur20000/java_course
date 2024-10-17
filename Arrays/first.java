eadffa80303620c9663a5827afd06dbf6b9061a5
mayur20000 <mayurvish2003@gmail.com>
2024-10-17 19:58:29 +0530

    first array, addres of remfereced

---
+++ Arrays/first.java
@@ -0,0 +1,12 @@
+package BasicsOfJava.Arrays;
+
+public class first {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        for (int i = 0; i < arr.length; i++)
+
+            System.out.println(arr[i]);
+
+
+    }
+}
