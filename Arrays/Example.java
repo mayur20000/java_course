16d1597d60cbd78636e42d37c8742041c4b82e47
mayur20000 <mayurvish2003@gmail.com>
2024-10-18 18:28:38 +0530

    exaple init,

---
+++ Arrays/Example.java
@@ -0,0 +1,27 @@
+
+
+
+package BasicsOfJava.Arrays;
+
+public class Example {
+    public static void main(String[] args) {
+        // Q: store a roll no
+
+        // Q: store a person's name
+        String name = "Mayur ";
+
+        //Q: store 5 roll no
+        int rno1 = 23;
+        int rno2 = 25;
+        int rno3 = 85;
+
+        int[] rnos = new int[5];
+        // or directly
+        int[] rnos2 = {23, 12, 56, 98, 75};
+
+        int[] ros; // declaration of array, ros is getting defines in stack
+        ros = new int[5]; // initializaation : actualy here object is being created in heap
+
+
+    }
+}
