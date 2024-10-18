3de649958aac367e445426213c0abb25858c7b3b
mayur20000 <mayurvish2003@gmail.com>
2024-10-18 18:28:03 +0530

    access by index

---
+++ Arrays/AccessByIndex.java
@@ -0,0 +1,18 @@
+package BasicsOfJava.Arrays;
+
+public class AccessByIndex {
+    public static void main(String[] args) {
+
+        int arr[] = new int[5];
+        System.out.println(arr[0]);
+
+        String arrr[] = new String[4];
+        System.out.println(arrr[0]);
+
+        int[] numbers = new int[5]; // Declares an array of integers with 5 elements
+        numbers[0] = 10; // Sets the first element to 10
+        int firstElement = numbers[0]; // Retrieves the first element
+
+
+    }
+}
