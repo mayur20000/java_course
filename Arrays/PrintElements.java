df670f29089f2f5039c8815cd581df332928b51c
mayur20000 <mayurvish2003@gmail.com>
2024-10-19 12:19:57 +0530

    Print all elements of array

---
+++ Arrays/PrintElements.java
@@ -0,0 +1,11 @@
+package BasicsOfJava.Arrays;
+
+public class PrintElements {
+    public static void main(String[] args) {
+        int array[] = {1, 2, 3, 4, 5};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+
+        }
+    }
+}
