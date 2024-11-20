f876b3c29d5837d23348934205af97de78688ef7
mayur20000 <mayurvish2003@gmail.com>
2024-11-20 15:38:55 +0530

    meathods

---
+++ ObjectAndClass/Customer.java
@@ -0,0 +1,32 @@
+package BasicsOfJava.ObjectAndClass;
+
+import java.sql.SQLOutput;
+
+public class Customer {
+    public String customerId;
+    public String customerName;
+    public long contactNumber;
+    public String address;
+
+    public void displaytCustomerDetails() {
+        System.out.println("Display Customer Detail \n");
+        System.out.println("Customer Id : " + customerId);
+        System.out.println("Customer Name : " + customerName);
+        System.out.println("Contact number : " + contactNumber);
+        System.out.println("Address :" + address);
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        customer.customerId = "C101";
+        customer.customerName = "John Doe";
+        customer.contactNumber = 7987660323L;
+        customer.address = "D089, St. Louis Street, Springfield, 62729";
+        customer.displaytCustomerDetails();
+        
+
+    }
+
+}
