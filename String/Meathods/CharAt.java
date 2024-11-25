f9064d8943158a8c34586b2e050358fbeffd5de9
mayur20000 <mayurvish2003@gmail.com>
2024-11-25 17:41:46 +0530

    string meathod to find char by index

---
+++ String/Meathods/CharAt.java
@@ -0,0 +1,10 @@
+package BasicsOfJava.String.Meathods;
+
+public class CharAt {
+    public static void main(String[] args) {
+        String s = new String("Hello");
+        System.out.print(s.charAt(0));
+
+        System.out.print(s.charAt(1));
+    }
+}
