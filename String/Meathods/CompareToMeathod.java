91cbfc265a77ff97e6ff4a44f7da978e28920f44
mayur20000 <mayurvish2003@gmail.com>
2024-11-25 18:24:01 +0530

    string meathod to compare two strign lexographically

---
+++ String/Meathods/CompareToMeathod.java
@@ -0,0 +1,10 @@
+package BasicsOfJava.String.Meathods;
+
+public class CompareToMeathod {
+    public static void main(String[] args) {
+        String s = new String("Hello");
+        String str = new String("hello");
+
+        System.out.println(s.compareTo(str));
+    }
+}
