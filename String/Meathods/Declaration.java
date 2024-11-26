15fc58d0bae43346c7cd9f2ecd7f69b279cde23b
mayur20000 <mayurvish2003@gmail.com>
2024-11-26 09:56:21 +0530

    How to declare String in java- which is of two type by new keyword and by string literal

---
+++ String/Meathods/Declaration.java
@@ -0,0 +1,19 @@
+package BasicsOfJava.String.Meathods;
+
+import java.util.*;
+
+public class Declaration {
+    public static void main(String[] args) {
+        String str = "Mayur"; // string literal
+        System.out.println(str);
+
+        String s = new String("Welcome");
+        System.out.println(s);
+
+        Scanner scan = new Scanner(System.in);
+        String st = scan.nextLine();
+        System.out.println(st);
+
+
+    }
+}
