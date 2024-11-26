1f4ff1f98f547ea257d207670f9caf22cf41b50a
mayur20000 <mayurvish2003@gmail.com>
2024-11-26 09:54:03 +0530

    contain meathod checks the string present that existing string ot not

---
+++ String/Meathods/ContainsMeathod.java
@@ -0,0 +1,10 @@
+package BasicsOfJava.String.Meathods;
+
+public class ContainsMeathod {
+    public static void main(String[] args) {
+
+        String s = new String("Hello");
+        boolean isPresent = s.contains("ell");
+        System.out.println(isPresent);
+    }
+}
