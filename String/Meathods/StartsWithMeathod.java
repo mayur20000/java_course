8758ed34e67eaa97d6b02114d72b488cbfe24c84
mayur20000 <mayurvish2003@gmail.com>
2024-11-26 09:59:57 +0530

    StartWith Meathod checks that existing strign start with particular char or not

---
+++ String/Meathods/StartsWithMeathod.java
@@ -0,0 +1,8 @@
+package BasicsOfJava.String.Meathods;
+
+public class StartsWithMeathod {
+    public static void main(String[] args) {
+        String s = new String("Hello");
+        System.out.println(s.startsWith("H"));
+    }
+}
