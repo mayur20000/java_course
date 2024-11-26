805c9c007fcbf1626bbeac920222c9fe1ef8dbcb
mayur20000 <mayurvish2003@gmail.com>
2024-11-26 10:11:38 +0530

    string convert to upper case

---
+++ String/Meathods/ToUpperCase.java
@@ -0,0 +1,9 @@
+package BasicsOfJava.String.Meathods;
+
+public class ToUpperCase {
+    public static void main(String[] args) {
+
+        String s = new String("hello");
+        System.out.println(s.toUpperCase());
+    }
+}
