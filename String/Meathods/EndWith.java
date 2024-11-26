bb027c3cb5613e96adf69d5a1531661976943171
mayur20000 <mayurvish2003@gmail.com>
2024-11-26 10:08:27 +0530

    endsWith check that, is string end wtih that particular char or string or not

---
+++ String/Meathods/EndWith.java
@@ -0,0 +1,10 @@
+package BasicsOfJava.String.Meathods;
+
+public class EndWith {
+    public static void main(String[] args) {
+
+        String s = new String("Hello");
+        boolean isEnd = false;
+        System.out.println(s.endsWith("lo"));
+    }
+}
