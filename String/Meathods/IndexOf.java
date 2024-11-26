9c6f6172a9a559cb1a80376aff915733fbdfde9e
mayur20000 <mayurvish2003@gmail.com>
2024-11-26 09:57:36 +0530

    IndexOf meathod provide the indexing of char

---
+++ String/Meathods/IndexOf.java
@@ -0,0 +1,8 @@
+package BasicsOfJava.String.Meathods;
+
+public class IndexOf {
+    public static void main(String[] args) {
+        String s = new String("Hello");
+        System.out.println(s.indexOf('l'));
+    }
+}
