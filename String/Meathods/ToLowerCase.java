afa441776dc2e8e1f98ff23ccf6bdb81e6ac6c86
mayur20000 <mayurvish2003@gmail.com>
2024-11-26 10:12:38 +0530

    ToLowerCase meathod convert string to lower case

---
+++ String/Meathods/ToLowerCase.java
@@ -0,0 +1,9 @@
+package BasicsOfJava.String.Meathods;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+
+        String s = new String("Hello");
+        System.out.println(s.toLowerCase());
+    }
+}
