6ae4531f8aab468e5a958147cf36d0f8be47304c
mayur20000 <mayurvish2003@gmail.com>
2024-11-26 10:01:04 +0530

    StringLength meathod provides the length of the string

---
+++ String/Meathods/StringLength.java
@@ -0,0 +1,12 @@
+package BasicsOfJava.String.Meathods;
+
+public class StringLength {
+    public static void main(String[] args) {
+        String s = new String("Hello");
+        System.out.println(s.length());
+
+
+        int length = s.length();
+        System.out.println(length);
+    }
+}
