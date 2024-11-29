554d063429a78fe0f858cc1f97ae8c6833b76619
mayur20000 <mayurvish2003@gmail.com>
2024-11-29 15:08:59 +0530

    first array program

--- Arrays/first.java
+++ Arrays/first.java
@@ -4,7 +4,6 @@ public class first {
     public static void main(String[] args) {
         int arr[] = new int[5];
         for (int i = 0; i < arr.length; i++)
-
             System.out.println(arr[i]);
 
 
