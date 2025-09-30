a2525c3bd09f81f30a09b32ddaa95ceac43963f7
mayur20000 <mayurvish2003@gmail.com>
2025-09-30 15:19:53 +0530

    explicit

---
+++ TakingInput/Explicit.java
@@ -0,0 +1,13 @@
+package BasicsOfJava.TakingInput;
+
+public class Explicit {
+    public static void main(String[] args) {
+        double dou = 9.78;
+        System.out.println();
+
+        int intValue = (int) dou;
+        System.out.println("conversion" + intValue);
+    }
+}
+
+
